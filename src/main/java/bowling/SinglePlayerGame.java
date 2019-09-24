43e2814dc20e4c1fede96467eca6af832c25e671
pedago <pedago@pcalb-mm0612.univ-jfc.fr>
2019-09-24 17:15:37 +0200

    class bowling

--- src/main/java/bowling/SinglePlayerGame.java
+++ src/main/java/bowling/SinglePlayerGame.java
@@ -7,6 +7,12 @@ package bowling;
  */
 public class SinglePlayerGame {
 
+    private int scoreTotal = 0;
+    private int tour = 1;
+    private int tourLimite = 12;
+    private int lastScore = 0;
+    private boolean spare = false;
+    private int strike = 0;
 	/**
 	 * Constructeur
 	 */
@@ -20,7 +26,37 @@ public class SinglePlayerGame {
 	 * ce lancé
 	 */
 	public void lancer(int nombreDeQuillesAbattues) {
-
+            if(this.tour <= this.tourLimite){
+                if(this.strike > 0){ //Si un strike a été fait lors des deux derniers lancers, on double le score de ce lancer
+                    this.scoreTotal += nombreDeQuillesAbattues;
+                    this.strike--;
+                }
+                if(nombreDeQuillesAbattues == 10){ //Le joueur fait un strike
+                    this.strike = 2;
+                    if(this.tour < 20){
+                       this.scoreTotal+=nombreDeQuillesAbattues; 
+                    }
+                }
+                else{
+                    this.tourLimite++;
+                    if(this.tour%2==1){ // S'il s'agit du 1er lancer de ce tour
+                        this.lastScore = nombreDeQuillesAbattues;
+                        this.scoreTotal += nombreDeQuillesAbattues;
+                        if(this.spare){ //Si un spare a été fait le tour précédent, on double le score de ce lancer
+                            this.scoreTotal += nombreDeQuillesAbattues;
+                            this.spare=false;
+                        }
+                    }
+                    else{ //S'il s'agit du second lancer de ce tour
+                        if(this.lastScore + nombreDeQuillesAbattues == 10){ //Le joueur fait un spare
+                            this.spare = true;
+                        }
+                        this.scoreTotal += nombreDeQuillesAbattues;
+                        this.lastScore = 0;
+                    }
+                }
+            this.tour++;
+            }
 	}
 
 	/**
@@ -29,6 +65,6 @@ public class SinglePlayerGame {
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+            return this.scoreTotal;
 	}
 }
